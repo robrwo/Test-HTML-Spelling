6a76496c03a7053bcf1eb8121fbbbbab20c686d9
Rob <rrwo@cpan.org>
2013-12-30 14:03:24 +0000

    Bumped version to v0.2.2

--- lib/Test/HTML/Spelling.pm
+++ lib/Test/HTML/Spelling.pm
@@ -102,7 +102,7 @@ use Scalar::Util qw( looks_like_number );
 use Search::Tokenizer;
 use Text::Aspell;
 
-use version 0.77; our $VERSION = version->declare('v0.2.1');
+use version 0.77; our $VERSION = version->declare('v0.2.2');
 
 # A placeholder key for the default spellchecker
 
